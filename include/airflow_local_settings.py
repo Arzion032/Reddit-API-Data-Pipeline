9d251294387c3351cf93cf3f80cec582626e3c28
Arzion032 <jmelvinsarabia032@gmail.com>
2025-10-22 20:27:56 +0800

    remove astro residue

--- include/airflow_local_settings.py
+++
@@ -1,9 +0,0 @@
-# include/airflow_local_settings.py
-from airflow.configuration import conf
-import os
-
-# Force CeleryExecutor at runtime
-os.environ["AIRFLOW__CORE__EXECUTOR"] = "CeleryExecutor"
-
-# Optional: print to confirm
-print(f"⚙️  Executor forced to {os.environ['AIRFLOW__CORE__EXECUTOR']}")
